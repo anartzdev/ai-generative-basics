8199c07249bbd3dde4f79bea7ceb673464ac5f9f
Anartz Mugika Ledo <105705996+anartzdev@users.noreply.github.com>
2025-03-12 13:42:47 +0100

    feat: new

---
+++ content/2/python/11.py
@@ -0,0 +1,5 @@
+import random as rnd
+
+for i in range(10):
+    print(rnd.randint(1,100))
+
---
+++ content/2/python/3.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+arr = np.array(range(1,11))
+result = arr[arr > 5]
+print(result)
